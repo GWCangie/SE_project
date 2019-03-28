ee45bb07aa476ff925389dd055bfb8f1ac16bea1
Jordan Garcia <jordangarcia@Jordans-MacBook-Pro.local>
2019-03-28 12:21:47 -0400

    First commit - feed page

---
+++ homePage.js
@@ -0,0 +1,51 @@
+			//changes save button when clicked
+			function change1()
+			{
+				var plus = '\u2295';
+				var minus = '\u2296';
+
+			    var sb = document.getElementById("saveButton1");
+			    if ( sb.innerText == plus ) 
+			    	sb.innerText = minus;
+			    else 
+			    	sb.innerText = plus;
+			}
+
+			//changes save button when clicked
+			function change2()
+			{
+				var plus = '\u2295';
+				var minus = '\u2296';
+
+			    var sb = document.getElementById("saveButton2");
+			    if ( sb.innerText == plus ) 
+			    	sb.innerText = minus;
+			    else 
+			    	sb.innerText = plus;
+			}
+
+			//changes save button when clicked
+			function change3()
+			{
+				var plus = '\u2295';
+				var minus = '\u2296';
+
+			    var sb = document.getElementById("saveButton3");
+			    if ( sb.innerText == plus ) 
+			    	sb.innerText = minus;
+			    else 
+			    	sb.innerText = plus;
+			}
+
+			//changes save button when clicked
+			function change4()
+			{
+				var plus = '\u2295';
+				var minus = '\u2296';
+
+			    var sb = document.getElementById("saveButton4");
+			    if ( sb.innerText == plus ) 
+			    	sb.innerText = minus;
+			    else 
+			    	sb.innerText = plus;
+			}
